de36dee113024140936512ec389369af567697ed
Melandr <melnychenko.a@gmail.com>
2023-03-21 11:53:27 +0200

    add inline style

--- src/CommentList.js
+++ src/CommentList.js
@@ -3,7 +3,7 @@ import Comment from "./Comment";
 
 export default function CommentList({ comments }) {
     const commentElements = comments.map((comment) => (
-        <li key={comment.id}>
+        <li key={comment.id} style={{ display: "inline-block" }}>
             <Comment comment={comment} />
         </li>
     ));
