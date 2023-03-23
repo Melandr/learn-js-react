c921a74b9b1b27c847f463fee6b9427103bd4562
Melandr <melnychenko.a@gmail.com>
2023-03-24 00:23:12 +0200

    publicPath -> "/"

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     output: {
         path: path.join(__dirname, "build"),
         filename: "bundle.js",
-        publicPath: "/static/",
+        publicPath: "/",
     },
     devServer: {
         historyApiFallback: true,
