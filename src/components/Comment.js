4fe800a1a48dfb6c14f03a5f1f7e5cf51959c61f
Melandr <melnychenko.a@gmail.com>
2023-03-23 23:58:33 +0200

    deploy for gh-pages

---
+++ src/components/Comment.js
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Comment({ comment }) {
+    return (
+        <div>
+            <p>
+                {comment.text}
+                <br />
+                <b>by {comment.user}</b>
+            </p>
+        </div>
+    );
+}
+
+Comment.propTypes = {
+    comment: PropTypes.shape({
+        text: PropTypes.string.isRequired,
+        user: PropTypes.string.isRequired,
+    }).isRequired,
+};
+
+export default Comment;
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require("path");
+
+module.exports = {
+    devtool: "source-map",
+    entry: ["./src/index.js"],
+    output: {
+        path: path.join(__dirname, "build"),
+        filename: "bundle.js",
+        publicPath: "/static/",
+    },
+    devServer: {
+        historyApiFallback: true,
+        static: {
+            directory: path.join(__dirname, "/"),
+        },
+        port: 8081,
+        open: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: ["babel-loader"],
+                include: path.join(__dirname, "src"),
+            },
+            {
+                test: /\.css/,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+};
