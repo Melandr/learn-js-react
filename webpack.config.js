1db4115c7a1d360ef037fc8f5667fe876ee0654f
Melandr <melnychenko.a@gmail.com>
2023-03-27 01:27:06 +0300

    move index.html into public

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.resolve("./index.html"),
+            template: path.join(__dirname, "public", "index.html"),
         }),
     ],
 };
