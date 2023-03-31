edd77dafd103d67bb1a525aa49fed78c3d076d8c
Melandr <melnychenko.a@gmail.com>
2023-03-31 13:36:24 +0300

    edit webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, "public", "index.html"),
+            template: "./public/index.html",
         }),
     ],
 };
