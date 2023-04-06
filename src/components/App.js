cee0f51952e3200a94fdff696de7b25d60de2ae4
Melandr <melnychenko.a@gmail.com>
2023-04-06 23:51:55 +0300

    add react-select

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,28 @@
 import PropTypes from "prop-types";
 import React, { Component } from "react";
 import ArticleList from "./ArticleList";
+import Select from "react-select";
 
 export default class App extends Component {
     static propTypes = {};
 
+    state = {
+        selection: null,
+    };
+
     render() {
+        const options = this.props.articles.map((article) => ({
+            label: article.title,
+            value: article.id,
+        }));
+
         return (
             <div>
+                <Select options={options} value={this.state.selection} onChange={this.changeSelection} />
                 <ArticleList articles={this.props.articles} />
             </div>
         );
     }
+
+    changeSelection = (selection) => this.setState({ selection });
 }
